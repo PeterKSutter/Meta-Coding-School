0b6b128fb66204a6a668b59bd3d9c4f35bb711ba
petersutter.pk@gmail.com <petersutter.pk@gmail.com>
2023-05-15 10:04:27 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var navbar = document.getElementById('menu');
+var close = document.getElementById('close');
+
+menu.addEventListener('click',function(){
+    document.getElementById("navbar").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("close").style.display = "block";
+
+})
+
+close.addEventListener('click',function(){
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("close").style.display = "none";
+
+})
